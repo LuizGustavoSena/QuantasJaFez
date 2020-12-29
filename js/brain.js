2e244ba7fadb6a25a43cefcab8030cb057112f78
LuizGustavoSena <luizgbs1@gmail.com>
2020-12-29 19:24:04 -0300

    2 geração

---
+++ js/brain.js
@@ -0,0 +1,101 @@
+var i = -1
+        var entregas = new Array()
+
+        function inicializar(){
+            adicionarLinha()
+            totalGanho()
+            qtdaPagas()
+        }
+
+        function adicionarLinha(){
+            i++
+            entregas[i] = definirMatriz(i)
+
+            criarLinhaTabela(entregas[i][0], entregas[i][1], entregas[i][2], i)
+
+            qtdaPagas()
+        };
+
+        function totalGanho(){
+            var j = i
+            var acumulador = 0
+            while (j >= 0){
+                if(entregas[j][2].checked){
+                    acumulador+= parseInt(entregas[j][0].value)
+                }
+                j--
+            }
+            acumulador = acumulador.toLocaleString('pt-BR', { style: 'currency', currency: 'BRL' })
+            document.getElementById("ptotal").innerHTML =  acumulador
+        }
+
+        function trocaCor(id) {
+            var cor = document.getElementById(id).style.backgroundColor
+            if(cor == 'rgb(255, 106, 106)') 
+                cor = '#00FF7F';
+            else 
+                cor = 'rgb(255, 106, 106)';
+            document.getElementById(id).style.background = cor;
+
+            totalGanho()
+            qtdaPagas()
+        }
+
+        function qtdaPagas(){
+            var j = i
+            var paga = 0
+            var npaga = 0
+            while(j >=0){
+                if(entregas[j][2].checked){
+                    paga += 1
+                }else{
+                    npaga += 1
+                }
+                j--
+            }
+            document.getElementById("ppagas").innerHTML = paga
+            document.getElementById("pnpagas").innerHTML = npaga
+        }
+
+        function definirMatriz(posicao){
+            var matriz = new Array();
+            matriz[0] = document.createElement("input")
+            matriz[1] = document.createElement("input")
+            matriz[2] = document.createElement('input')
+
+            matriz[0].setAttribute("type", "number")
+            matriz[0].setAttribute("id", "valor") 
+            matriz[0].setAttribute("class", "form-control")  
+            matriz[0].setAttribute("placeholder", "R$ 0,00")  
+
+            matriz[1].setAttribute("type", "text")
+            matriz[1].setAttribute("id", "local")
+            matriz[1].setAttribute("class", "form-control")
+            matriz[1].setAttribute("placeholder", "Descrição do local de entrega")  
+
+            matriz[2].setAttribute("type", "checkbox")
+            matriz[2].setAttribute("id", "pago")
+            matriz[2].setAttribute("onclick", "trocaCor(" + posicao + ")")
+
+            return matriz;
+        }
+
+        function criarLinhaTabela(valor, local, pago, posicao) {
+            var tabela = document.getElementById("tabela");
+            var linhas = tabela.rows.length;
+            var colunas = tabela.rows[linhas-1].cells.length;
+            var novaLinha = tabela.insertRow(linhas);
+            novaLinha.setAttribute("style", "background-color:#FF6A6A")
+            novaLinha.setAttribute("id", posicao)
+
+            for (var j = 0; j < colunas; j++) {
+                novaCelula = novaLinha.insertCell(j);
+                if (j == 0){
+                    novaCelula.appendChild(valor)
+                }else if (j == 1){
+                    novaCelula.appendChild(local)
+                }else if (j == 2){
+                    novaCelula.appendChild(pago)
+                }
+            }
+        }
