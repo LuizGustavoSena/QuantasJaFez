6b30b0a401eb3dfe72fd7a834801df600109f422
LuizGustavoSena <luizgbs1@gmail.com>
2021-01-17 00:08:02 -0300

    2.5 geração

--- js/banco.js
+++ js/banco.js
@@ -1,14 +1,42 @@
 function abreConexao(){
     this.bdSql = openDatabase('Quantasjafez', '1.0', 'BD Quantasjafez', 2*1024*1024 )
-    console.log(this.bdSql)
 }
 
-abreConexao.prototype.listahistorico = function(){
+abreConexao.prototype.criarTabela = function(){
     this.bdSql.transaction(function(tx){
-        tx.executeSql('create table if not exists entregas(id INTEGER AUTO_INCREMENT NOT NULL, valor number NOT NULL, descricao TEXT', [[]], function(tx, msg){
-            console.log("sucesso" + msg)
-        }, function(err){
-            console.log("Erro " + err)
+        tx.executeSql('CREATE TABLE if not exists entregas(id INTEGER NOT NULL, valor number NOT NULL, descricao TEXT, pago number)', [], function(tx, msg){
+        }, function(tx, err){
+            console.log("Erro criar tabela " + err)
+        })
+    })
+}
+
+abreConexao.prototype.inserir = function(id, valor, descricao, pago){
+    this.bdSql.transaction(function(tx){
+        tx.executeSql('INSERT INTO entregas(id, valor , descricao, pago ) values(?, ?, ?, ?)', [id, valor, descricao, pago], function(tx, msg){
+        }, function(tx, err){
+            console.log("erro inserir" + err)
+        })
+    })
+}
+
+abreConexao.prototype.atualizar = function(id, val, desc, pag){
+    this.bdSql.transaction(function(tx){
+        tx.executeSql('UPDATE entregas SET descricao = ?, valor = ?, pago = ? WHERE id = ?', [desc, val, pag, id], function(tx, msg){
+            //console.log("Atualizado")
+        }, function(tx, err){
+            console.log("Erro atualizar " + err)
+        })
+    })
+}
+
+abreConexao.prototype.listar = function(callback){
+    this.bdSql.transaction(function(tx){
+        tx.executeSql('SELECT * FROM entregas', [], function(tx, msg){
+            callback(msg)
+           // console.log("Sucesso listagem")
+        }, function(tx, err){
+            console.log("Erro seleção" + err)
         })
     })
 }
--- js/brain.js
+++ js/brain.js
@@ -1,111 +1,210 @@
 var i = -1
-        var entregas = new Array()
+var entregas = new Array()
+var bd = new abreConexao()
 
-        function inicializar(){
-            adicionarLinha()
-            totalGanho()
-            qtdaPagas()
+$(document).ready( function(){
+    bd.criarTabela()
+    $('corpo').empty()
+    headTable()
+    bd.listar(listaEntregas)
+    totalGanho()
+    qtdaPagas()
+})
 
-            var bd = new abreConexao()
-            bd.listahistorico()
+function listaEntregas(msg){
+    for(var j = 0; j < msg.rows.length; j++){
+        i++
+        entregas[i] = definirMatriz(msg.rows.item(j)[['id']], msg.rows.item(j)[['valor']], msg.rows.item(j)[['descricao']], msg.rows.item(j)[['pago']])
+        criarLinhaTabela(entregas[i][0], entregas[i][1], entregas[i][2], i)
+        eventCheckbox(i)
+    }
+
+    if(i == -1){
+        adicionarLinha()
+    }
+}
+
+function addBanco(posicao, valor, local, pago){
+
+    var existe = false
+    if(posicao > 0){
+        for(var j = 0; j < entregas.length; j++){
+            if(entregas[j])
         }
+            if(entregas[posicao - 1][0].value == entregas[posicao][0].value &&
+                entregas[posicao - 1][1].value == entregas[posicao][1].value){
+                existe = true
+                j = entregas.length
+            }
+    }
+        if(existe){
+            console.log("Linha existente" + posicao)
+                bd.atualizar(posicao, valor, local, pago)
+        }else{
+            console.log("Linha nova" + posicao)
+            bd.inserir(posicao, valor, local, pago)
+        }
+}
 
-        function adicionarLinha(){
-            if(i == -1 || entregas[i][0].value != ""){
-                i++
-                entregas[i] = definirMatriz(i)
+function adicionarLinha(){
+    if(i == -1 || entregas[i][0].value != ""){
 
-                criarLinhaTabela(entregas[i][0], entregas[i][1], entregas[i][2], i)
-
-                qtdaPagas()
+        if(i >= 0){
+            if(entregas[i][2].checked){
+                var pago = 1
             }else{
-                alert("Cadastre na linha já existente")
+                var pago = 0
             }
-        };
-
-        function totalGanho(){
-            var j = i
-            var acumulador = 0
-            while (j >= 0){
-                if(entregas[j][2].checked && entregas[j][0].value != ""){
-                    acumulador+= parseInt(entregas[j][0].value)
-                }
-                j--
-            }
-            acumulador = acumulador.toLocaleString('pt-BR', { style: 'currency', currency: 'BRL' })
-            document.getElementById("ptotal").innerHTML =  acumulador
+            addBanco(i, entregas[i][0].value, entregas[i][1].value, entregas[i][2].value)
         }
+        
+        i++
+        entregas[i] = definirMatriz(i, null, null, null)
+        criarLinhaTabela(entregas[i][0], entregas[i][1], entregas[i][2], i)
+        
+        qtdaPagas()
+        
+    }else{
+        alert("Cadastre na linha já existente")
+    }
+};
 
-        function eventCheckbox(id) {
-            var cor = document.getElementById(id).style.backgroundColor
-            if(entregas[id][2].checked){
-                document.getElementById(id).style.background = '#00FF7F'
-                entregas[id][0].readOnly = true;
-                entregas[id][1].readOnly = true;
+function totalGanho(){
+    var j = i
+    var acumulador = 0
+
+    while (j >= 0){
+        if(entregas[j][2].checked && entregas[j][0].value != ""){
+            acumulador+= parseInt(entregas[j][0].value)
+        }
+        j--
+    }
+
+    acumulador = acumulador.toLocaleString('pt-BR', { style: 'currency', currency: 'BRL' })
+
+    document.getElementById("ptotal").innerHTML =  acumulador
+}
+
+function eventCheckbox(id) {
+    if(entregas[id][2].checked ){  // Marcado
+        document.getElementById(id).style.background = '#00FF7F'
+        entregas[id][0].readOnly = true
+        entregas[id][1].readOnly = true
+        entregas[id][2].setAttribute("value", 1)
+    }else{  // Desmarcado
+        document.getElementById(id).style.background = 'rgb(255, 106, 106)'
+        entregas[id][0].readOnly = false
+        entregas[id][1].readOnly = false
+        entregas[id][2].setAttribute("value", 0)
+    }
+
+    //if(roda == 1){
+        addBanco(id, entregas[id][0].value, entregas[id][1].value, entregas[id][2].value)
+    //}
+    totalGanho()
+    qtdaPagas()
+}
+
+function qtdaPagas(){
+        var j = i
+        var paga = 0
+        var npaga = 0
+
+        while(j >=0){
+            if(entregas[j][2].checked){
+                paga += 1
             }else{
-                document.getElementById(id).style.background = 'rgb(255, 106, 106)'
-                entregas[id][0].readOnly = false;
-                entregas[id][1].readOnly = false;
+                npaga += 1
             }
-            totalGanho()
-            qtdaPagas()
+            j--
         }
 
-        function qtdaPagas(){
-            var j = i
-            var paga = 0
-            var npaga = 0
-            while(j >=0){
-                if(entregas[j][2].checked){
-                    paga += 1
-                }else{
-                    npaga += 1
-                }
-                j--
-            }
-            document.getElementById("ppagas").innerHTML = paga
-            document.getElementById("pnpagas").innerHTML = npaga
+        document.getElementById("ppagas").innerHTML = paga
+        document.getElementById("pnpagas").innerHTML = npaga
+    }
+
+function definirMatriz(posicao, valor, descricao, pago){
+    var matriz = new Array();
+
+    matriz[0] = document.createElement("input")
+    matriz[1] = document.createElement("input")
+    matriz[2] = document.createElement('input')
+
+    matriz[0].setAttribute("type", "number")
+    matriz[0].setAttribute("id", "valor")
+    if(valor != null){
+        matriz[0].setAttribute("value", valor)
+    } 
+    matriz[0].setAttribute("class", "form-control")  
+    matriz[0].setAttribute("placeholder", "R$ 0,00") 
+
+    matriz[1].setAttribute("type", "text")
+    matriz[1].setAttribute("id", "local")
+    if(descricao != null){
+        matriz[1].setAttribute("value", descricao)
+    }
+    matriz[1].setAttribute("class", "form-control")
+    matriz[1].setAttribute("placeholder", "Descrição do local de entrega")  
+
+    matriz[2].setAttribute("type", "checkbox")
+    matriz[2].setAttribute("id", "pago")
+    matriz[2].setAttribute("value", 0)
+    if(pago == 1){
+        matriz[2].checked = true
+    }
+    matriz[2].setAttribute("onclick", "eventCheckbox(" + posicao + ")")
+
+    return matriz;
+}
+
+function headTable(){
+    var table = document.createElement("table")
+    var thead = document.createElement("thead")
+    var tr = document.createElement("tr")
+    var thv = document.createElement("th")
+    var thl = document.createElement("th")
+    var thp = document.createElement("th")
+
+    table.setAttribute("id","tabela")
+    table.setAttribute("class","table")
+    table.append(thead)
+    thead.append(tr)
+
+    thv.setAttribute("scope","col")
+    thv.setAttribute("id","tvalor")
+    thv.append(document.createTextNode("Valor"))
+    tr.append(thv)
+
+    thl.setAttribute("scope","col")
+    thl.setAttribute("id","tlocal")
+    thl.append(document.createTextNode("Local"))
+    tr.append(thl)
+
+    thp.setAttribute("scope","col")
+    thp.setAttribute("id","tpago")
+    thp.append(document.createTextNode("Pago"))
+    tr.append(thp)
+
+    document.getElementById("corpo").append(table)
+}
+
+function criarLinhaTabela(valor, local, pago, posicao) {
+    var tabela = document.getElementById("tabela");
+    var linhas = tabela.rows.length;
+    var colunas = tabela.rows[linhas-1].cells.length;
+    var novaLinha = tabela.insertRow(linhas);
+
+    novaLinha.setAttribute("style", "background-color:#FF6A6A")
+    novaLinha.setAttribute("id", posicao)
+
+    for (var j = 0; j < colunas; j++) {
+        novaCelula = novaLinha.insertCell(j);
+        if (j == 0){
+            novaCelula.appendChild(valor)
+        }else if (j == 1){
+            novaCelula.appendChild(local)
+        }else if (j == 2){  
+            novaCelula.appendChild(pago)
         }
-
-        function definirMatriz(posicao){
-            var matriz = new Array();
-            matriz[0] = document.createElement("input")
-            matriz[1] = document.createElement("input")
-            matriz[2] = document.createElement('input')
-
-            matriz[0].setAttribute("type", "number")
-            matriz[0].setAttribute("id", "valor") 
-            matriz[0].setAttribute("class", "form-control")  
-            matriz[0].setAttribute("placeholder", "R$ 0,00")   
-
-            matriz[1].setAttribute("type", "text")
-            matriz[1].setAttribute("id", "local")
-            matriz[1].setAttribute("class", "form-control")
-            matriz[1].setAttribute("placeholder", "Descrição do local de entrega")  
-
-            matriz[2].setAttribute("type", "checkbox")
-            matriz[2].setAttribute("id", "pago")
-            matriz[2].setAttribute("onclick", "eventCheckbox(" + posicao + ")")
-
-            return matriz;
-        }
-
-        function criarLinhaTabela(valor, local, pago, posicao) {
-            var tabela = document.getElementById("tabela");
-            var linhas = tabela.rows.length;
-            var colunas = tabela.rows[linhas-1].cells.length;
-            var novaLinha = tabela.insertRow(linhas);
-            novaLinha.setAttribute("style", "background-color:#FF6A6A")
-            novaLinha.setAttribute("id", posicao)
-
-            for (var j = 0; j < colunas; j++) {
-                novaCelula = novaLinha.insertCell(j);
-                if (j == 0){
-                    novaCelula.appendChild(valor)
-                }else if (j == 1){
-                    novaCelula.appendChild(local)
-                }else if (j == 2){
-                    novaCelula.appendChild(pago)
-                }
-            }
-        }
+    }
+}
